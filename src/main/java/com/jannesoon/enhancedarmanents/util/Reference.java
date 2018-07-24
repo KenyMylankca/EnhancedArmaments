8cd8015fbc6594eb8f7366e3543891d129463cd8
JanneSoon <jannesoon@hotmail.com>
2018-07-24 17:31:22 +0300

    version 1.1.0

--- src/main/java/com/jannesoon/enhancedarmanents/util/Reference.java
+++ src/main/java/com/jannesoon/enhancedarmanents/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MODID = "enhancedarmanents";
 	public static final String NAME = "Enhanced Armanents";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.0";
 	public static final String COMMON = "com.jannesoon.enhancedarmanents.proxies.CommonProxy";
 	public static final String CLIENT = "com.jannesoon.enhancedarmanents.proxies.ClientProxy";
 }
