4406a3b429736c878da11c8de9b23b1b68aa2722
JanneSoon <jannesoon@hotmail.com>
2018-07-24 18:27:25 +0300

    version 1.1.1

--- src/main/java/com/jannesoon/enhancedarmaments/util/Reference.java
+++ src/main/java/com/jannesoon/enhancedarmaments/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MODID = "enhancedarmaments";
 	public static final String NAME = "Enhanced Armaments";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 	public static final String COMMON = "com.jannesoon.enhancedarmaments.proxies.CommonProxy";
 	public static final String CLIENT = "com.jannesoon.enhancedarmaments.proxies.ClientProxy";
 }
