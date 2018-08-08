4bb96dc9a788932fcd69ade2d65f5fbde783017f
JanneSoon <41585837+JanneSoon@users.noreply.github.com>
2018-08-08 17:16:15 +0300

    1.2.5

--- src/main/java/com/jannesoon/enhancedarmaments/EnhancedArmaments.java
+++ src/main/java/com/jannesoon/enhancedarmaments/EnhancedArmaments.java
@@ -35,7 +35,7 @@ public class EnhancedArmaments
 {
 	public static final String MODID = "enhancedarmaments";
 	public static final String NAME = "Enhanced Armaments";
-	public static final String VERSION = "1.2.4";
+	public static final String VERSION = "1.2.5";
 	public static final String COMMON = "com.jannesoon.enhancedarmaments.proxies.CommonProxy";
 	public static final String CLIENT = "com.jannesoon.enhancedarmaments.proxies.ClientProxy";
 	
