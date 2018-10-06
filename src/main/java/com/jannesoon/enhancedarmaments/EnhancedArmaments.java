4e01c8b4840cb294968be640ca2accc869882290
KenyMylankca <ahmet.yilmaz1995@windowslive.com>
2018-10-06 22:46:35 +0300

    1.3.2

--- src/main/java/com/jannesoon/enhancedarmaments/EnhancedArmaments.java
+++ src/main/java/com/jannesoon/enhancedarmaments/EnhancedArmaments.java
@@ -35,7 +35,7 @@ public class EnhancedArmaments
 {
 	public static final String MODID = "enhancedarmaments";
 	public static final String NAME = "Enhanced Armaments";
-	public static final String VERSION = "1.3.1";
+	public static final String VERSION = "1.3.2";
 	public static final String COMMON = "com.jannesoon.enhancedarmaments.proxies.CommonProxy";
 	public static final String CLIENT = "com.jannesoon.enhancedarmaments.proxies.ClientProxy";
 	
