ceea0529cf7b9046b471c5e3027377a05d90f91e
KenyMylankca <ahmet.yilmaz1995@windowslive.com>
2019-09-05 11:25:25 +0300

    Revert "maybe works... key error is still there"
    This reverts commit 41f0de0e47e6b18bdfba668653205e4e2215f685.

--- src/main/java/com/jannesoon/enhancedarmaments/event/EventItemTooltip.java
+++ src/main/java/com/jannesoon/enhancedarmaments/event/EventItemTooltip.java
@@ -1,7 +1,9 @@
 package com.jannesoon.enhancedarmaments.event;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+
+import org.lwjgl.input.Keyboard;
 
 import com.google.common.collect.Multimap;
 import com.jannesoon.enhancedarmaments.config.Config;
@@ -11,8 +13,6 @@ import com.jannesoon.enhancedarmaments.essentials.Rarity;
 import com.jannesoon.enhancedarmaments.util.EAUtils;
 import com.jannesoon.enhancedarmaments.util.NBTHelper;
 
-import net.java.games.input.Component.Identifier.Key;
-import net.java.games.input.Keyboard;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
@@ -21,33 +21,28 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * Displays information about the weapon when hovered over in an inventory.
  *
  */
-@Mod.EventBusSubscriber
 public class EventItemTooltip 
 {
 	/**
 	 * Gets called whenever the tooltip for an item needs to appear.
 	 * @param event
 	 */
-	@OnlyIn(Dist.CLIENT)
+	@SideOnly(Side.CLIENT)
 	@SubscribeEvent(priority = EventPriority.HIGH)
 	public void addInformation(ItemTooltipEvent event)
 	{
-		List<ITextComponent> tooltip = (List<ITextComponent>) event.getToolTip();
+		ArrayList<String> tooltip = (ArrayList<String>) event.getToolTip();
 		ItemStack stack = event.getItemStack();
 		Item item = stack.getItem();
 
@@ -70,28 +65,28 @@ public class EventItemTooltip
 					
 					// level
 					if (level >= Config.maxLevel)
-						tooltip.add(new TextComponentString(I18n.format("enhancedarmaments.misc.level") + ": " + TextFormatting.RED + I18n.format("enhancedarmaments.misc.max")));
+						tooltip.add(I18n.format("enhancedarmaments.misc.level") + ": " + TextFormatting.RED + I18n.format("enhancedarmaments.misc.max"));
 					else
-						tooltip.add(new TextComponentString(I18n.format("enhancedarmaments.misc.level") + ": " + TextFormatting.WHITE + level));
+						tooltip.add(I18n.format("enhancedarmaments.misc.level") + ": " + TextFormatting.WHITE + level);
 					
 					// experience
 					if (level >= Config.maxLevel)
-						tooltip.add(new TextComponentString(I18n.format("enhancedarmaments.misc.experience") + ": " + I18n.format("enhancedarmaments.misc.max")));
+						tooltip.add(I18n.format("enhancedarmaments.misc.experience") + ": " + I18n.format("enhancedarmaments.misc.max"));
 					else
-						tooltip.add(new TextComponentString(I18n.format("enhancedarmaments.misc.experience") + ": " + experience + " / " + maxExperience));
+						tooltip.add(I18n.format("enhancedarmaments.misc.experience") + ": " + experience + " / " + maxExperience);
 					
 					// durability
 					if (Config.showDurability)
 					{
-						tooltip.add(new TextComponentString(I18n.format("enhancedarmaments.misc.durability") + ": " + (stack.getMaxDamage() - stack.getDamage()) + " / " + stack.getMaxDamage()));
+						tooltip.add(I18n.format("enhancedarmaments.misc.durability") + ": " + (stack.getMaxDamage() - stack.getItemDamage()) + " / " + stack.getMaxDamage());
 					}
 					
 					// abilities
-					tooltip.add(new TextComponentString(""));
-					if (Keyboard.isKeyDown(Key.LSHIFT))
+					tooltip.add("");
+					if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT))
 					{
-						tooltip.add(new TextComponentString(rarity.getColor() + "" + TextFormatting.ITALIC + I18n.format("enhancedarmaments.misc.abilities")));
-						tooltip.add(new TextComponentString(""));
+						tooltip.add(rarity.getColor() + "" + TextFormatting.ITALIC + I18n.format("enhancedarmaments.misc.abilities"));
+						tooltip.add("");
 						
 						if (EAUtils.canEnhanceWeapon(item))
 						{
@@ -99,7 +94,7 @@ public class EventItemTooltip
 							{
 								if (ability.hasAbility(nbt))
 								{
-									tooltip.add(new TextComponentTranslation("-" + ability.getColor() + ability.getName(nbt)));
+									tooltip.add("-" + ability.getColor() + ability.getName(nbt));
 								}
 							}
 						}
@@ -109,25 +104,24 @@ public class EventItemTooltip
 							{
 								if (ability.hasAbility(nbt))
 								{
-									tooltip.add(new TextComponentTranslation("-" + ability.getColor() + ability.getName(nbt)));
+									tooltip.add("-" + ability.getColor() + ability.getName(nbt));
 								}
 							}
 						}
 					}
 					else
-						tooltip.add(new TextComponentString(rarity.getColor() + "" + TextFormatting.ITALIC + I18n.format("enhancedarmaments.misc.abilities.shift")));
+						tooltip.add(rarity.getColor() + "" + TextFormatting.ITALIC + I18n.format("enhancedarmaments.misc.abilities.shift"));
 				}
 			}
 		}
 	}
 	
-	private void changeTooltips(List<ITextComponent> tooltip, ItemStack stack, Rarity rarity)
+	private void changeTooltips(ArrayList<String> tooltip, ItemStack stack, Rarity rarity)
 	{
 		// rarity after the name
-		tooltip.set(0, new TextComponentString(stack.getDisplayName() + rarity.getColor() + " (" + TextFormatting.ITALIC + I18n.format("enhancedarmaments.rarity." + rarity.getName()) + ")"));
+		tooltip.set(0, stack.getDisplayName() + rarity.getColor() + " (" + TextFormatting.ITALIC + I18n.format("enhancedarmaments.rarity." + rarity.getName()) + ")");
 		
-		
-		if (tooltip.stream().filter(text -> text.getString().equals("When in Main Hand")).findAny().map(tooltip::indexOf).get() != -1 && !(stack.getItem() instanceof ItemBow))
+		if (tooltip.indexOf("When in main hand:") != -1 && !(stack.getItem() instanceof ItemBow))
 		{
 			Multimap<String, AttributeModifier> map = stack.getItem().getAttributeModifiers(EntityEquipmentSlot.MAINHAND, stack);
 			Collection<AttributeModifier> damageCollection = map.get(SharedMonsterAttributes.ATTACK_DAMAGE.getName());
@@ -136,29 +130,25 @@ public class EventItemTooltip
 			String d = String.format("%.1f", damage);
 			
 			if(rarity.getEffect() != 0)
-				tooltip.set(tooltip.stream().filter(text -> text.getString().equals("When in Main Hand")).findAny().map(tooltip::indexOf).get() + 2, new TextComponentString(rarity.getColor() + " " + d + TextFormatting.GRAY +" "+ I18n.format("enhancedarmaments.misc.tooltip.attackdamage")));
+				tooltip.set(tooltip.indexOf("When in main hand:") + 2, rarity.getColor()+" " + d + TextFormatting.GRAY +" "+ I18n.format("enhancedarmaments.misc.tooltip.attackdamage"));
 		}
-		
-		if (tooltip.stream().filter(text -> text.getString().equals("When on head")).findAny().map(tooltip::indexOf).get() != -1
-				|| tooltip.stream().filter(text -> text.getString().equals("When on body")).findAny().map(tooltip::indexOf).get() != -1
-				|| tooltip.stream().filter(text -> text.getString().equals("When on legs")).findAny().map(tooltip::indexOf).get() != -1
-				|| tooltip.stream().filter(text -> text.getString().equals("When on feet")).findAny().map(tooltip::indexOf).get() != -1)
+		if (tooltip.indexOf("When on head:") != -1 || tooltip.indexOf("When on body:") != -1 || tooltip.indexOf("When on legs:") != -1 || tooltip.indexOf("When on feet:") != -1)
 		{
 			String p = String.format("%.1f", 100-(100/(1.0F + (rarity.getEffect()/5F))));
 			float percentage = Float.valueOf(p);
 			int line = 2;
-			if(tooltip.stream().filter(text -> text.getString().equals("When on head")).findAny().map(tooltip::indexOf).get() != -1) line = tooltip.stream().filter(text -> text.getString().equals("When on head")).findAny().map(tooltip::indexOf).get();
-			if(tooltip.stream().filter(text -> text.getString().equals("When on body")).findAny().map(tooltip::indexOf).get() != -1) line = tooltip.stream().filter(text -> text.getString().equals("When on body")).findAny().map(tooltip::indexOf).get();
-			if(tooltip.stream().filter(text -> text.getString().equals("When on legs")).findAny().map(tooltip::indexOf).get() != -1) line = tooltip.stream().filter(text -> text.getString().equals("When on legs")).findAny().map(tooltip::indexOf).get();
-			if(tooltip.stream().filter(text -> text.getString().equals("When on feet")).findAny().map(tooltip::indexOf).get() != -1) line = tooltip.stream().filter(text -> text.getString().equals("When on feet")).findAny().map(tooltip::indexOf).get();
+			if(tooltip.indexOf("When on head:") != -1) line = tooltip.indexOf("When on head:");
+			if(tooltip.indexOf("When on body:") != -1) line = tooltip.indexOf("When on body:");
+			if(tooltip.indexOf("When on legs:") != -1) line = tooltip.indexOf("When on legs:");
+			if(tooltip.indexOf("When on feet:") != -1) line = tooltip.indexOf("When on feet:");
 			if(percentage != 0)
-				tooltip.add(line + 1, new TextComponentString(" " + TextFormatting.BLUE + "+" + rarity.getColor() + percentage + TextFormatting.BLUE + "% " + I18n.format("enhancedarmaments.misc.rarity.armorreduction")));
+				tooltip.add(line + 1," " + TextFormatting.BLUE + "+" + rarity.getColor() + percentage + TextFormatting.BLUE + "% " + I18n.format("enhancedarmaments.misc.rarity.armorreduction"));
 		}
 		
 		if(EAUtils.canEnhanceRanged(stack.getItem()) && rarity.getEffect() != 0)
 		{
 			String b = String.format("%.1f", rarity.getEffect()/3*100);
-			tooltip.add(1, new TextComponentString(I18n.format("enhancedarmaments.misc.rarity.arrowpercentage") + " " + rarity.getColor() + "+" + b + "%"));
+			tooltip.add(1, I18n.format("enhancedarmaments.misc.rarity.arrowpercentage") + " " + rarity.getColor() + "+" + b + "%");
 		}
 	}
 }
