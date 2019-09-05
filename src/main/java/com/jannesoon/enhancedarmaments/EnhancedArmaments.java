4d07bbc5485210162b69928ae5fa5d2d35a255e7
KenyMylankca <ahmet.yilmaz1995@windowslive.com>
2019-09-05 11:24:18 +0300

    Revert "1.4.1 to be released"
    This reverts commit f1104efa4cd060782556a64fdfcad804f3d8b9a4.

--- src/main/java/com/jannesoon/enhancedarmaments/EnhancedArmaments.java
+++ src/main/java/com/jannesoon/enhancedarmaments/EnhancedArmaments.java
@@ -39,7 +39,7 @@ public class EnhancedArmaments
 	public static final String MODID = "enhancedarmaments";
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 	private static final String PROTOCOL_VERSION = "1.0";
-	public static final String VERSION = "1.4.1";
+	public static final String VERSION = "1.4.0";
 
 	public static final ISidedProxy proxy = DistExecutor.runForDist(() -> ClientProxy::new, null);
 
