d3e76b2d2b1cadccd31bbefa0f2e09223308b70a
KenyMylankca <ahmet.yilmaz1995@windowslive.com>
2020-04-05 20:26:49 +0300

    1.3.19

--- src/main/java/com/kenymylankca/enhancedarmaments/EnhancedArmaments.java
+++ src/main/java/com/kenymylankca/enhancedarmaments/EnhancedArmaments.java
@@ -35,7 +35,7 @@ public class EnhancedArmaments
 {
 	public static final String MODID = "enhancedarmaments";
 	public static final String NAME = "Enhanced Armaments";
-	public static final String VERSION = "1.3.18";
+	public static final String VERSION = "1.3.19";
 	public static final String COMMON = "com.kenymylankca.enhancedarmaments.proxies.CommonProxy";
 	public static final String CLIENT = "com.kenymylankca.enhancedarmaments.proxies.ClientProxy";
 	
